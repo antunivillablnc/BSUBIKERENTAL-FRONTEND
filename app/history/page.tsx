5b108414df246b490003f0ea7e08dd975a68dfa7
Anthony <22-38317@g.batstate-u.edu.ph>
2025-11-10 23:56:35 +0800

    remove serverside cookie for history user

--- app/history/page.tsx
+++ app/history/page.tsx
@@ -1,13 +1,6 @@
-import { cookies } from 'next/headers';
-import { redirect } from 'next/navigation';
 import HistoryClient from './HistoryClient';
 
 export default async function AccountHistoryPage() {
-	const cookieStore = await cookies();
-	const auth = cookieStore.get('auth')?.value;
-	if (!auth) {
-		redirect('/');
-	}
 	return <HistoryClient />;
 }
 
