04887a48cdea4155676972e43be677a1a3d78f84
Anthony <22-38317@g.batstate-u.edu.ph>
2025-11-10 08:19:11 +0800

    better logo

--- app/layout.tsx
+++ app/layout.tsx
@@ -18,8 +18,8 @@ export const metadata: Metadata = {
   title: "SPARTA | BSU-TNEU Bike Rental",
   description: "Online bike rental system for Batangas State University students.",
   icons: {
-    icon: "/logo-spartan.png",          // or your custom path
-    apple: "/logo-spartan.png",
+    icon: "/logo-spartan-square.png",          // or your custom path
+    apple: "/logo-spartan-square.png",        // optional
   }
 };
 
